ade1cbb863022cf4b0fdd216846c37688a6218b9
Yuhi-Kikuchi620409 <snow.1ove-1ife-_-0409@ezweb.ne.jp>
2019-06-06 15:06:35 +0900

    Initial Commit

---
+++ TableViewApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp2
+//
+//  Created by 菊地雄飛 on 2019/06/06.
+//  Copyright © 2019 菊地雄飛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
